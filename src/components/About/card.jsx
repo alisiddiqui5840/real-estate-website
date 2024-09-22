fe1fcdb15740b54d534edf0c98dd24970b258c9d
alisiddiqui5840 <91695748+alisiddiqui5840@users.noreply.github.com>
2024-09-22 18:09:35 +0500

    fix issue of deployment

--- src/components/About/card.jsx
+++ src/components/About/card.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from 'react';
 import Image from 'next/image';
-import Modal from './Modal';
+import Modal from './modal';
 
 const Card = ({ imageSrc, title, tiltDirection, onClick }) => {
   return (
